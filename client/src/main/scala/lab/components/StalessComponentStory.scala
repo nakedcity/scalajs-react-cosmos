d35bd8aa3e5025b4caef50be4d8b8d4e39bb3449
Camilo Azula <camiloandres.azulaconde@nike.com>
2018-11-29 17:55:35 +0100

    Remove dictionary instead using ComponentProps

--- client/src/main/scala/lab/components/StalessComponentStory.scala
+++ client/src/main/scala/lab/components/StalessComponentStory.scala
@@ -2,14 +2,13 @@ package lab.components
 
 
 import lab.lib.reactcosmos.Fixture
-
-import scala.scalajs.js.Dictionary
+import lab.components.StatelessComponent.ComponentProps
 
 object StalessComponentStory {
   val fixture = new Fixture(
     name = "Stateless Component",
-    component = StatelessComponent.component.toJsComponent.raw ,
-    props = Dictionary("content" -> "Hello")
+    component = StatelessComponent.component.toJsComponent.raw,
+    props = ComponentProps(content = "hello 2")
   )
 
 }
